0e57ea375009474ec399a66a8e342dc0c7d047cb
Aliny Melquiades <alinymelquiadesdesouza@gmail.com>
2024-08-02 13:06:09 -0300

    Update Pessoa.java

--- src/main/java/com/springboot/crudspring/model/Pessoa.java
+++ src/main/java/com/springboot/crudspring/model/Pessoa.java
@@ -1,2 +1,26 @@
-package com.springboot.crudspring.model;public class Pessoa {
+package com.springboot.crudspring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pessoa {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 15)
+    private String fone;
 }
